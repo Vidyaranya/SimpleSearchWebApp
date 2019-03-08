3610fb2b399f285d7fd901d224e96449d44b7dea
Vidyaranya Sai <vidyaranya.ns@gmail.com>
2019-03-08 00:11:10 +0000

    working version

--- src/Search.java
+++ src/Search.java
@@ -1,5 +1,7 @@
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -12,31 +14,60 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 
-@WebServlet(urlPatterns = {"/Search"})
+/**
+ * A class which contains the logic to find the names matching the given name passed from the jsp
+ */
+@WebServlet(urlPatterns = {
+        "/Search"
+})
 public class Search extends HttpServlet {
 
-    public void service(HttpServletRequest request,
-                           HttpServletResponse response) throws ServletException,
-            IOException {
+    // A constant which is the query parameter passed from the jsp
+    final String searchParameter = "name";
+    // A constant which is the key containing the field that we are searching. For now both this and searchParameter
+    // are same. However, in the future their might be changes in one of the files and it's better to have different
+    // parameters for each of them for maintainability.
+    final String jsonKeyToSearch = "name";
+
+    /**
+     * Gets the search parameter, finds the appopriate names from the json and returns the matching values.
+     * @param request Request received from the jsp. Contains the name to be searched.
+     * @param response Response returned which contains the name(s) matching the given input name.
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("hello");
+
         final String filePath = "/WEB-INF/exercise.json";
         ServletContext context = this.getServletContext();
-        String pathname =context.getRealPath(filePath);
-        String searchString = request.getParameter("name");
-        FileReader reader = new FileReader(pathname);
+        String pathname = context.getRealPath(filePath);
 
+        String searchString = request.getParameter(searchParameter);
+        FileReader reader = new FileReader(pathname);
         JSONParser jsonParser = new JSONParser();
+        boolean isMatchFound = false;
+
         try {
-            JSONArray jsonObject = (JSONArray) jsonParser.parse(reader);
-            Iterator iterator = jsonObject.iterator();
+            JSONArray employeeData = (JSONArray) jsonParser.parse(reader);
+            Iterator iterator = employeeData.iterator();
             while (iterator.hasNext()) {
-                out.println(iterator.next());
+                JSONObject individualEmployee = (JSONObject) iterator.next();
+                String currentEmployeeName = (String) individualEmployee.get(searchParameter);
+                if (currentEmployeeName.toLowerCase().contains(searchString.toLowerCase())) {
+                    out.println(currentEmployeeName);
+                    isMatchFound = true;
+                }
             }
-        } catch (Exception e) {
+            if (!isMatchFound) {
+                out.println("No match found");
+            }
+        } catch (ParseException e) {
             e.printStackTrace();
         }
-
-
+        out.close();
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,16 +1,17 @@
-<%--
-
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-    <form action="Search" method="get">
-    Enter the name to search : <input type ="text" name="name">
+
+<head>
+  <title>Search Page</title>
+</head>
+
+<body>
+<form action="Search" method="get">
+  Enter the name to search :
+  <input type="text" name="name">
   <br>
   <input type="submit" value="Search">
-    </form>
-  </body>
+</form>
+</body>
+
 </html>
